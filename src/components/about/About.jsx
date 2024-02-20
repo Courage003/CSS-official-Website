f33d82802e2023fe21d00d13f316ad49eb3ea487
Satwik Gupta <satwikgupta1801@gmail.com>
2024-02-21 03:43:44 +0530

    top about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,51 +4,49 @@ import Card from "../UI/Card";
 
 export default function About() {
   return (
-    <section className="container w-full transition">
+    <section className="container w-screen transition">
       <div
         className="mb-12 w-screen p-20 text-center text-white"
         style={{ background: "#000" }}
       >
         <h4 className="text-4xl font-bold">#KnowUs</h4>
-        <p>
-        EMPOWER. INSPIRE. TRANSFORM.
-        </p>
-        </div>
-        <div className="w-screen">
-        <div className="mb-20 flex items-center justify-center flex-col lg:flex-row">
-            <div className="m-12 lg:ml-8 lg:mr-4 rounded-lg lg:w-1/2">
+        <p>EMPOWER. INSPIRE. TRANSFORM.</p>
+      </div>
+      <div className="w-screen">
+        <div className="mb-20 flex flex-col items-center justify-center lg:flex-row">
+          <div className="m-10 rounded-lg lg:ml-8 lg:mr-4 lg:w-1/2">
             <img src={grp} alt="grpPhoto" style={{ borderRadius: "12px" }} />
-            </div>
-            <div className="ml-1 flex lg:w-1/2 flex-col items-center justify-center">
+          </div>
+          <div className="ml-1 flex flex-col items-center justify-center lg:w-1/2">
             <h4 className="text-center text-4xl font-bold">Who We Are ?</h4>
-            <p className="tracking-lg w-fit px-16 lg:px-12 lg:pr-16 pt-4 text-lg leading-6 ">
-                Civil Service Society, NIT Agartala serves as a dynamic platform
-                for impassioned students to rigorously examine, deliberate upon,
-                and actively engage with current public Policy and governance
-                issues. Encouraging critical thinking and fostering innovative
-                ideation, we foster collaborative efforts among students. Our
-                overarching goal is to empower students with the requisite
-                knowledge, skills, and resources to effectuate meaningful change
-                in the realm of public service. Through robust discourse and
-                concerted action, we endeavor to cultivate future leaders
-                dedicated to shaping a more informed, effective, and equitable
-                governance landscape.
+            <p className="tracking-lg w-fit px-16 pt-4 text-lg leading-6 lg:px-12 lg:pr-16 ">
+              Civil Service Society, NIT Agartala serves as a dynamic platform
+              for impassioned students to rigorously examine, deliberate upon,
+              and actively engage with current public Policy and governance
+              issues. Encouraging critical thinking and fostering innovative
+              ideation, we foster collaborative efforts among students. Our
+              overarching goal is to empower students with the requisite
+              knowledge, skills, and resources to effectuate meaningful change
+              in the realm of public service. Through robust discourse and
+              concerted action, we endeavor to cultivate future leaders
+              dedicated to shaping a more informed, effective, and equitable
+              governance landscape.
             </p>
-            <br />
+            {/* <br />
 
-            <br />
+            <br /> */}
             {/* <marquee loop="-1" scrollAmount="5" width="20%" className="bg-slate-700 text-white">
                 Create stunning images with as much or as little control as you
                 like thanks to a choice of Basic and Creative modes.
             </marquee> */}
-            </div>
+          </div>
         </div>
-        </div>
+      </div>
 
-        <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
         <h1 className="text-5xl font-semibold underline">Our Teams</h1>
-        <Card/>
-        </div>
+        <Card />
+      </div>
     </section>
-);
+  );
 }
