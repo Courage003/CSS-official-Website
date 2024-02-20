4a97d0e6572373e0e53fd1b8b0a14916ef95657a
Satwik Gupta <satwikgupta1801@gmail.com>
2024-02-20 14:20:01 +0530

    Update Sidemenu.jsx

--- src/components/header/Sidemenu.jsx
+++ src/components/header/Sidemenu.jsx
@@ -1,43 +1,44 @@
-import React from "react";
+import React from "react"
+import {Link} from "react-router-dom"
 
 export default function Sidemenu() {
   return (
     <ul className="space-y-1 ">
       <li>
-        <a
-          href="#"
+        <Link
+          to="/about"
           className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
         >
           About Us
-        </a>
+        </Link>
       </li>
 
       <li>
-        <a
-          href="#"
+        <Link
+          to="/career"
           className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
         >
           Career and Exams
-        </a>
+        </Link>
       </li>
 
 
       <li>
-        <a
-          href="#"
+        <Link
+          to="/resources"
           className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
         >
           Resources
-        </a>
+        </Link>
       </li>
 
       <li>
-        <a
-          href="#"
+        <Link
+          to="/events"
           className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
         >
           Events
-        </a>
+        </Link>
       </li>
     </ul>
   );
