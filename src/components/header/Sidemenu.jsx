ad676d432666239a20a4f91938dfb00aae022396
Satwik Gupta <satwikgupta1801@gmail.com>
2024-02-20 21:39:34 +0530

    sidemenu about button

--- src/components/header/Sidemenu.jsx
+++ src/components/header/Sidemenu.jsx
@@ -7,7 +7,7 @@ export default function Sidemenu() {
       <li>
         <Link
           to="/about"
-          className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
+          className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
         >
           About Us
         </Link>
