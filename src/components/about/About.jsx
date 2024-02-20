70e407d52f5de4bd47fd60aad6da09125f44f1f5
Lalit Jangir <lalitkumarjangir02@gmail.com>
2024-02-21 01:50:17 +0530

    issue fixed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,14 +3,14 @@ import grp from "../../assets/grpPhoto.jpg";
 
 export default function About() {
   return (
-    <section className="container w-screen transition">
+    <section className="container w-full transition">
       <div
-        className="mb-12 w-screen p-20 text-center text-white"
+        className="mb-12 w-full p-20 text-center text-white"
         style={{ background: "#000" }}
       >
-        <h4 className="text-5xl font-bold">#KnowUs</h4>
+        <h4 className="text-4xl font-bold">#KnowUs</h4>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, porro?
+        EMPOWER. INSPIRE. TRANSFORM.
         </p>
       </div>
       <div className="w-screen">
@@ -19,30 +19,30 @@ export default function About() {
             <img src={grp} alt="grpPhoto" style={{ borderRadius: "12px" }} />
           </div>
           <div className="ml-1 flex lg:w-1/2 flex-col items-center justify-center">
-            <h4 className="text-center text-6xl font-bold">Who We Are ?</h4>
+            <h4 className="text-center text-4xl font-bold">Who We Are ?</h4>
             <p className="tracking-lg w-fit px-16 lg:px-12 lg:pr-16 pt-4 text-lg leading-6 ">
-              Civil Service Society, NIT Agartala serves as a dynamic platform
-              for impassioned students to rigorously examine, deliberate upon,
-              and actively engage with current public Policy and governance
-              issues. Encouraging critical thinking and fostering innovative
-              ideation, we foster collaborative efforts among students. Our
-              overarching goal is to empower students with the requisite
-              knowledge, skills, and resources to effectuate meaningful change
-              in the realm of public service. Through robust discourse and
-              concerted action, we endeavor to cultivate future leaders
-              dedicated to shaping a more informed, effective, and equitable
-              governance landscape.
+                Civil Service Society, NIT Agartala serves as a dynamic platform
+                for impassioned students to rigorously examine, deliberate upon,
+                and actively engage with current public Policy and governance
+                issues. Encouraging critical thinking and fostering innovative
+                ideation, we foster collaborative efforts among students. Our
+                overarching goal is to empower students with the requisite
+                knowledge, skills, and resources to effectuate meaningful change
+                in the realm of public service. Through robust discourse and
+                concerted action, we endeavor to cultivate future leaders
+                dedicated to shaping a more informed, effective, and equitable
+                governance landscape.
             </p>
             <br />
 
             <br />
-            <marquee loop="-1" scrollamount="5" width="100%">
-              Create stunning images with as much or as little control as you
-              like thanks to a choice of Basic and Creative modes.
-            </marquee>
-          </div>
+            {/* <marquee loop="-1" scrollAmount="5" width="20%" className="bg-slate-700 text-white">
+                Create stunning images with as much or as little control as you
+                like thanks to a choice of Basic and Creative modes.
+            </marquee> */}
+            </div>
         </div>
-      </div>
+        </div>
 
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-5xl font-semibold underline">Our Teams</h1>
