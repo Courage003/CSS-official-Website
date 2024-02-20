e3f7fc77e88c40a56cf10ba83d0f6bc08275b281
Lalit Jangir <lalitkumarjangir02@gmail.com>
2024-02-20 17:46:13 +0530

    some minor changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -15,11 +15,11 @@ export default function About() {
                     </div>
                     <div className="w-1/2 ml-1 flex flex-col justify-center items-center">
                         <h4 className="text-6xl text-center font-bold">Who We Are ?</h4>
-                        <p className="leading-5 text-lg pt-4 w-fit ">Civil Service Society, NIT Agartala serves as a dynamic platform for impassioned students to rigorously examine, deliberate upon, and actively engage with current public Policy and governance issues. Encouraging critical thinking and fostering innovative ideation, we foster collaborative efforts among students. Our overarching goal is to empower students with the requisite knowledge, skills, and resources to effectuate meaningful change in the realm of public service. Through robust discourse and concerted action, we endeavor to cultivate future leaders dedicated to shaping a more informed, effective, and equitable governance landscape.</p>
+                        <p className="leading-6 tracking-lg text-lg pt-4 px-16 w-fit ">Civil Service Society, NIT Agartala serves as a dynamic platform for impassioned students to rigorously examine, deliberate upon, and actively engage with current public Policy and governance issues. Encouraging critical thinking and fostering innovative ideation, we foster collaborative efforts among students. Our overarching goal is to empower students with the requisite knowledge, skills, and resources to effectuate meaningful change in the realm of public service. Through robust discourse and concerted action, we endeavor to cultivate future leaders dedicated to shaping a more informed, effective, and equitable governance landscape.</p>
                 <br/>
+                
                 <br/>
-                <br/>
-                {/* <marquee loop="-1" scrollamount="5" width="100%" >Create stunning images with as much or as little control as you like thanks to a choice of Basic and Creative modes.</marquee> */}
+                <marquee loop="-1" scrollamount="5" width="100%" >Create stunning images with as much or as little control as you like thanks to a choice of Basic and Creative modes.</marquee>
                     </div>
                 </div>
             </div>
