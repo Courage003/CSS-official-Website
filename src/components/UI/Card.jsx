cb97c728d8c422cfb5ff29bebc1c70509328481b
Lalit Jangir <lalitkumarjangir02@gmail.com>
2024-02-23 23:16:43 +0530

    card hover

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -2,37 +2,60 @@ import React from "react";
 import { people } from "../../data";
 
 export default function Card() {
-    return (
+  return (
     <div className="w-full bg-white py-24 sm:py-32">
-        <div className="mx-auto max-w-7xl 0 px-6 lg:px-8 ">
+      <div className="0 mx-auto max-w-7xl px-6 lg:px-8 ">
         <ul
-            role="list"
-            className="grid items-center justify-center grid-cols-2 gap-x-8 gap-y-12 sm:gap-y-16 md:grid-cols-3"
+          role="list"
+          className="grid grid-cols-2 items-center justify-center gap-x-8 gap-y-12 sm:gap-y-16 md:grid-cols-3"
         >
-            {people.map((person) => (
+          {people.map((person) => (
             <li key={person.id} className="mb-4">
-                <div className="flex flex-col items-center overflow-hidden rounded-lg cursor-pointer
-                transition ease-in-out delay-100 hover:shadow-lg hover:shadow-black-100">
-                
-                    <img
-                    className="overflow-hidden object-cover object-top mt-4 h-64 w-60 rounded-xl  border border-gray-400"
+              <div className="flex flex-col items-center overflow-hidden rounded-lg transition delay-100 ease-in-out">
+                <div className="group relative">
+                  <img
+                    className="h-64 w-60 transform overflow-hidden rounded-xl border border-gray-400 object-cover object-top transition duration-300 ease-in-out hover:scale-105"
                     src={person.imageUrl}
                     alt=""
-                    />
-                
+                  />
+                  <div className="absolute inset-0 flex items-center justify-center rounded-xl bg-black bg-opacity-50 opacity-0 transition duration-500 ease-in-out group-hover:opacity-100">
+                    <div className="flex items-center space-x-4">
+                      <a
+                        href={person.linkedinUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-white hover:text-blue-500"
+                      >
+                        <i
+                          className="fa fa-linkedin pr-2 text-xl cursor-pointer"
+                          aria-hidden="true"
+                        ></i>
+                      </a>
+                      <a
+                        href={person.twitterUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-white hover:text-slate-400"
+                      >
+                        <i className="fa-brands fa-x-twitter pr-2 text-xl cursor-pointer"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+
                 <div className="mt-3 text-center">
-                    <h3 className="text-base font-semibold leading-7 text-gray-900">
+                  <h3 className="text-base font-semibold leading-7 text-gray-900">
                     {person.name}
-                    </h3>
-                    <p className="text-sm font-semibold leading-6 text-indigo-600">
+                  </h3>
+                  <p className="text-sm font-semibold leading-6 text-indigo-600">
                     {person.role}
-                    </p>
+                  </p>
                 </div>
-                </div>
+              </div>
             </li>
-            ))}
+          ))}
         </ul>
-        </div>
+      </div>
     </div>
-    );
+  );
 }
