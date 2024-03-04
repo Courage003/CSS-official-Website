835c3917d235fcd16631c3bfee07bf6829649756
Lalit Jangir <lalitkumarjangir02@gmail.com>
2024-03-04 22:17:57 +0530

    scrollup issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
 import { Header, Footer, Loading, } from "./components/index";
 import ScrollUp from "./components/ScrollUp";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
       <Suspense fallback={<Loading />}>
+        <ScrollToTop/>
         <Header />
         <main>
           <Outlet />
